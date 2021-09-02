578e9ec8d7e9dc276e9f3baf25ff7eb537c72ca7
Julie Burgoon <julieburgoon@Julies-Air.domain>
2021-09-01 18:03:14 -0600

    finished

--- madlibs/src/Madlib.js
+++ madlibs/src/Madlib.js
@@ -4,17 +4,17 @@ import './Madlib.css'
 const MadLib = () =>{
     const [madLibInfo, setMadLibInfo] = useState()
     const [toggle, setToggle] = useState(false)
-    
+    //adds the form data to the parents state
     const addMadLib = (data) =>{
         setMadLibInfo(data)
         setToggle(true)
     }
-
+//resets the madlib
     const handleCLick = () =>{
         setToggle(false)
 
     }
-
+//set the form to show up if toggle is set to false, set the h3 and button to show up when toggle is set to true
     return (
         <div>
             
--- madlibs/src/MadlibForm.js
+++ madlibs/src/MadlibForm.js
@@ -10,18 +10,18 @@ const MadLibForm = ({add}) =>{
      }
     const [madLibInfo, setMadLibInfo] = useState(INITIAL_STATE)
     
-    
+    //updates the form state when anyhting changes
     const handleChange = (e) =>{
         const {name, value} = e.target;
         return setMadLibInfo({...madLibInfo, [name]:value})
     }
-
+//call the add function from the parent to pass the state up and then resets the form
     const handleSubmit =(e) =>{
         e.preventDefault();
         add(madLibInfo)
         setMadLibInfo(INITIAL_STATE)
     }
-
+//renders the form to create the madlib
     return (
       <form onSubmit={handleSubmit}>
           <label htmlFor="noun">Noun:</label>
